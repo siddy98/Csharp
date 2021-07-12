b6a486232380b97d0fb14924c7fc55fb30177c29
siddy98 <87292874+siddy98@users.noreply.github.com>
2021-07-12 09:43:44 +0530

    Add files via upload

---
+++ Assignment4/Assignment4/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Contact
+    {
+        public int contactNo { get; set;}
+        public string ContactName { get; set; }
+        public string cellno { get; set; }
+    }
+}
